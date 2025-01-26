dde96e464437b420bf6d35b6b1f2485bd71a353f
Vansh Gilhotra <vanshgilhotra00992@gmail.com>
2025-01-26 20:38:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const apikey = "6efb73201ccd4414c0220af12963507c";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+
+
+
+async function checkWeather(city){
+   const response = await fetch(apiurl + city + `&appid=${apikey}`);
+
+   if(response.status == 404){
+      document.querySelector(".error").style.display="block";
+      document.querySelector(".weather").style.display = "none";
+   }
+   else{
+      var data = await response.json();
+      
+      document.querySelector(".weather").style.display = "block";
+      document.querySelector(".error").style.display="none";
+
+      document.querySelector(".city-name").innerHTML = data.name; 
+      document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+      document.querySelector(".humidity").innerHTML = data.main.humidity + "%" ;
+      document.querySelector(".wind").innerHTML = data.wind.speed +"km/h";
+   }
+}
+
+searchbtn.addEventListener("click",()=>{
+   checkWeather(searchbox.value);
+})
